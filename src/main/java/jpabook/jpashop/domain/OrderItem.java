d699c7b928169dd39a86ceddffdc15727353e86b
minseok980601 <minseok980601@gmail.com>
2022-09-05 23:09:42 +0900

    20220905 Study

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,13 +1,16 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -24,4 +27,29 @@ public class OrderItem {
 
     private int orderPrice; // 주문 가격
     private int count;      // 주문 수량량
+
+    // ==생성 메서드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        // 넘어 온 것 만큼 아이템의 재고를 깍음
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    // ==비즈니스 로직==
+    // 재고수량을 롬복해준다.
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    // ==조회 로직==
+    // 주문 상품 전체 가격 조회
+    // 주문할 때 주문 가격과 수량이 있으므로 두개를 곱해야 한다.
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            // id 값이 없으면 새로 생성하는 객체이다. 신규로 등록한다.
+            em.persist(item);
+        } else {
+            // 아이템 값이 있으면 db 에 등록된걸 가져오는 것이므로 업데이트 한다.
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
